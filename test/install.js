cece70b4758b8bd1b9edcc7f8c3a1f15eda89e5b
vaultec81 <47548474+vaultec81@users.noreply.github.com>
2020-11-16 11:05:22 -0800

    add multiple platforms to github actions

--- test/install.js
+++ test/install.js
@@ -2,7 +2,7 @@ const goIPFS = require('../src/index');
 const assert = require('assert');
 const fs = require('fs');
 describe("go-ipfs", function() {
-    this.timeout(60000);
+    this.timeout(240000);
     it("Install", async() => {
         var str = goIPFS.getDefaultPath();
         const realPath = await goIPFS.install()
