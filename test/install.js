77c86f9351dc6855d172b8258028f3325e5bde66
vaultec81 <47548474+vaultec81@users.noreply.github.com>
2020-11-17 21:38:10 -0800

    feat: use temp paths for tests

--- test/install.js
+++ test/install.js
@@ -1,12 +1,14 @@
 const goIPFS = require('../src/index');
 const assert = require('assert');
 const fs = require('fs');
+const Tmp = require('tmp');
+const Path = require('path')
 describe("go-ipfs", function() {
     this.timeout(240000);
     it("Install", async() => {
-        var str = goIPFS.getDefaultPath();
-        const realPath = await goIPFS.install()
-        assert.strictEqual(realPath, str)
+        var tempPath = Path.join(Tmp.dirSync().name, "ipfs");
+        const realPath = await goIPFS.install({installPath:tempPath})
+        assert.strictEqual(realPath, tempPath)
         assert.strictEqual(fs.existsSync(realPath), true)
     })
 })
